962916d668a9155eb8b156b0b6b46074f37b66de
Elliass Rachidi <elliass@Elliasss-MacBook-Pro.local>
2023-08-11 12:36:14 +0200

    Changed file structure

--- src/scheduling/solver.py
+++ src/scheduling/solver.py
@@ -1,6 +1,6 @@
 from z3 import Or, Sum, Optimize, sat
-from schedule import TSCHSchedule
-from slotframe import Slotframe
+from scheduling.schedule import TSCHSchedule
+from scheduling.slotframe import Slotframe
 import time
 
 class TSCHSolver:
